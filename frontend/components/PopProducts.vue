c358185b9dc66db4fe5f190bd6bf0e4ba3f54c0a
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-02-25 00:32:09 +0500

    FIX: fixed

--- frontend/components/PopProducts.vue
+++ frontend/components/PopProducts.vue
@@ -3,14 +3,14 @@
         <Splide :options="{ gap: 5, autoplay: true, perPage : 4, pagination: false, autoheight: true  }"  v-if="is_product" aria-label="My Favorite Images">
             <SplideSlide v-for="product in getData" :key="product.id">
                 <div class="card main-card">
-                    <NuxtLink :to="{ name: 'product-id', params: { id: product.id } }" class="me-auto ms-auto"><img :src="baseUrl + product.image" style="margin-top: 20px; padding: 0;" class="img-fluid" :alt="product.name" ></NuxtLink>
+                    <NuxtLink :to="{ name: 'product-id', params: { id: product.slug } }" class="me-auto ms-auto"><img :src="baseUrl + product.image" style="margin-top: 20px; padding: 0;" class="img-fluid" :alt="product.name" ></NuxtLink>
                     <div class="card-body">
-                        <NuxtLink :to="{ name: 'product-id', params: { id: product.id } }" class="nav-link">
+                        <NuxtLink :to="{ name: 'product-id', params: { id: product.slug } }" class="nav-link">
                             <h4 class="card-title">{{ product.name }} - {{ product.weight }}</h4>
                             <span class="card-price">{{ product.price }} сум</span>
                         </NuxtLink>
                         <div>
-                            <nuxt-link :to="{ name: 'product-id', params: { id: product.id } }" class="btn btn-success mt-2 btn-sm"><span>Перейти</span></nuxt-link>
+                            <nuxt-link :to="{ name: 'product-id', params: { id: product.slug } }" class="btn btn-success mt-2 btn-sm"><span>Перейти</span></nuxt-link>
                         </div>
                     </div>
                 </div>
