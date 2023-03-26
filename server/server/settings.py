90de45e41638a286fc96612dc10a98f17a2f86b8
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-03-26 18:26:36 +0300

    CHANGE: changed media root

--- server/server/settings.py
+++ server/server/settings.py
@@ -140,8 +140,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT = '/home/Jalyuzi/server/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = '/home/Jalyuzi/server/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
