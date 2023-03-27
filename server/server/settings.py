007f26a6d874819400651412bcbc190e3c22b58d
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-03-27 21:43:24 +0500

    ADD: added postgresql

--- server/server/settings.py
+++ server/server/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '.db/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jalyuzi',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
