b32134a081945b897d725e1783bc7fea913f0151
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-07-29 13:58:32 +0500

    CHANGE: changed gunicorn config

--- server/scripts/start_prod.sh
+++ server/scripts/start_prod.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn --workers=1 --worker-class=gthread --log-level critical --timeout=30 --threads=4 server.wsgi -b 0.0.0.0:8000 
+gunicorn --workers=1 --worker-class=gthread --log-level info --timeout=60 --threads=1 server.wsgi -b 0.0.0.0:8000 
