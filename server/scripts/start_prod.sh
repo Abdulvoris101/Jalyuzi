26af6c7aa36e478afde0660fc0813891ebc72585
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-07-29 14:30:41 +0500

    CHANGE: changed gunicorn to uwsgi

--- server/scripts/start_prod.sh
+++ server/scripts/start_prod.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn --workers=1 --log-level info --timeout=60 server.wsgi -b 0.0.0.0:8000 
+uwsgi --ini uwsgi.ini
